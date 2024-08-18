f77c058ee2ce3acf6df7509a43c9b91e7d8ac9fe
NaglaFathi11 <naglafatehi11@gmail.com>
2024-08-18 10:27:46 +0200

    changee

--- src/AllComponents/ReplyInputComponent/ReplyInput.jsx
+++ src/AllComponents/ReplyInputComponent/ReplyInput.jsx
@@ -1,31 +1,38 @@
-import './ReplyInput.css'
-import { useState } from'react';
+import "./ReplyInput.css";
+import { useState } from "react";
 
 export default function ReplyInput(props) {
-      const [replyContent, setReplyContent] = useState(`@${props.UserName} `);
-    
-      function handleReplyChange(event) {
-          setReplyContent(event.target.value);
-        }
+  const [replyContent, setReplyContent] = useState(`@${props.UserName} `);
 
-      function handleSendReply() {
-          const cleanedReplyContent = replyContent.startsWith(`@${props.UserName}`) ? replyContent.substring(`@${props.UserName} `.length)
-            : replyContent;
-          
-            props.handleAddReply(cleanedReplyContent, props.commentId);
-            setReplyContent('');
-            props.setShowReplyInput(false);   
-      // console.log(replyContent)
-      //  console.log( props.commentId)
-        }
+  function handleReplyChange(event) {
+    setReplyContent(event.target.value);
+  }
+
+  function handleSendReply() {
+    const cleanedReplyContent = replyContent.startsWith(`@${props.UserName}`)
+      ? replyContent.substring(`@${props.UserName} `.length)
+      : replyContent;
+
+    props.handleAddReply(cleanedReplyContent, props.commentId);
+    setReplyContent("");
+    props.setShowReplyInput(false);
+    // console.log(replyContent)
+    //  console.log( props.commentId)
+  }
+
+  const isReplyContentEmpty = replyContent == `@${props.UserName} `;
 
   return (
-    <div id='ReplyInput'>
-            <img src={props.CurrentUserImage} alt="Not Found" id='NaglaUserAvatar' />
-            <textarea value={replyContent} onChange={handleReplyChange} />
-            <button onClick={handleSendReply}>Send</button>
+    <div id="ReplyInput">
+      <img src={props.CurrentUserImage} alt="Not Found" id="NaglaUserAvatar" />
+      <textarea value={replyContent} onChange={handleReplyChange} />
+      <button
+        onClick={handleSendReply}
+        disabled={isReplyContentEmpty}
+        className={isReplyContentEmpty ? "disabledSend" : null}
+      >
+        Send
+      </button>
     </div>
-  )
+  );
 }
-
- 
