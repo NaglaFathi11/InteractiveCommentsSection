8848400a545160a673d8d263ff9e56cb4eed767d
NaglaFathi11 <naglafatehi11@gmail.com>
2024-08-18 10:06:29 +0200

    change

--- src/AllComponents/AddNewCommentComponent/AddNewComment.jsx
+++ src/AllComponents/AddNewCommentComponent/AddNewComment.jsx
@@ -1,23 +1,34 @@
-import { useState } from 'react'
-import './AddNewComment.css'
+import { useState } from "react";
+import "./AddNewComment.css";
 
- export default function AddNewComment(props) {
+export default function AddNewComment(props) {
+  const [ValueInput, setValueInput] = useState("");
 
-    const [ValueInput, setValueInput] = useState('')
+  function HandleSendComment() {
+    props.CommentAdded(ValueInput);
+    setValueInput("");
+  }
 
-    function HandleSendComment(){
-        props.CommentAdded(ValueInput)
-        setValueInput('')
-    }
-
-    function HandleChangeComment (event){ 
-        setValueInput(event.target.value) 
-    }
+  function HandleChangeComment(event) {
+    setValueInput(event.target.value);
+  }
 
   return (
-   <div id='NewComment'>
-        <img src={props.CurrentUserImage} alt="Not Found"/>
-        <textarea type="text" placeholder='Add a comment...' value={ValueInput} onChange={HandleChangeComment}/>
-        <button onClick={HandleSendComment}>SEND</button>
-   </div>
-  )}
+    <div id="NewComment">
+      <img src={props.CurrentUserImage} alt="Not Found" />
+      <textarea
+        type="text"
+        placeholder="Add a comment..."
+        value={ValueInput}
+        onChange={HandleChangeComment}
+      />
+      <button
+        onClick={HandleSendComment}
+        disabled={ValueInput ? false : true}
+        className={ValueInput ? "activeBTN" : "disabledBTN"}
+      >
+        SEND
+      </button>
+    </div>
+  );
+}
